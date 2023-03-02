25311fdd05a89d9baf72ce9b54e525ba212f387c
FelipeAFV <ffigueroa@whitestack.com>
2023-03-02 14:34:41 -0300

    commit

--- terraform.tf
+++ terraform.tf
@@ -1,4 +1,61 @@
+/*****************
+ * Configuration *
+******************/
+/* NB: If this number changes from 3, the section resource "openstack_networking_port_v2" "whitemist_ports" in main.tf must be updated to */
+variable amountsIpsWhiteMist{
+  type = number
+  default = 3
+}
 
+variable amountsIpsNotWhiteMist{
+  type = number
+  default = 1
+}
+
+/* Total amount of nodes to be created */
+variable amountNodes {
+  type = number
+  default = 1
+}
+
+/* Amount of admin nodes to be created */
+variable amountAdminNodes {
+  type = number
+  default = 1
+}
+
+/* Obtain imageName from command-line */
+variable imageName {
+  type = string
+  default = "Ubuntu 20.04 LTS"
+}
+
+variable nodeFlavour {
+  type = string
+  default = "m1.medium"
+}
+
+/* Obtain project id from command-line */
+variable projectId {
+  type = string
+}
+
+/* Obtain project id from command-line */
+variable modulePicker {
+  type = number
+}
+
+variable prefix {
+  type = string
+  default = ""
+}
+
+/* SSH timeout */
+variable timeout {
+  type = number
+  default = 120
+}
+ 
 resource "template_dir" "prepare_local_whitecloud_config" {
   source_dir = "etc/whitecloud/"
   destination_dir = "/tmp/whitecloud/"
