e7694fce6d6a634c1d7c9c21c286403eaa8ef756
FelipeAFV <ffigueroa@whitestack.com>
2023-03-02 14:36:24 -0300

    commit

--- terraform.tf
+++ terraform.tf
@@ -61,6 +61,10 @@ resource "template_dir" "prepare_local_whitecloud_config" {
   destination_dir = "/tmp/whitecloud/"
   vars = {
     controlAsCompute = var.controlAsCompute
+    ipNodesString = join(",", var.ipNodes)
+    amountAdminNodes = var.amountAdminNodes
+    ipVip = var.ipVip
+    prefix = var.prefix
   }
 }
 
